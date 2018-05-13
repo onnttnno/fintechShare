62b7bec925769b751ab203acd3a459fb27c76cc1
Ritthirong Phoomane <sanamori@msn.com>
2018-05-14 03:09:35 +0700

    kk

--- server.js
+++ server.js
@@ -168,7 +168,12 @@ app.post('/node/fintechShare/secure/handShake/', function (req, res) {
     console.log("log data : " + chipher);
     //var data = Buffer.from(Object.keys(chipher)[0], 'base64');
     //var forgeBuffer = forge.util.createBuffer(data.toString('base64'));
-    var decrypted = keystore.privateKey.decrypt(new Buffer(chipher,'base64'), 'RSA-OAEP');
+    var decrypted = keystore.privateKey.decrypt(chipher, 'RSA-OAEP', {
+        md: forge.md.sha256.create(),
+        mgf1: {
+            md: forge.md.sha1.create()
+        }
+    });
     console.log("decypted data : "+decrypted);
     /* var cypher =  pki.publicKeyFromPem(Object.keys(chipher));
     var decrypted = keystore.privateKey.decrypt(cypher, 'RSA-OAEP', {
