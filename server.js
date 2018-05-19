a448bc7330935ee7aedb1de4268bfb4e44a64655
Ritthirong Phoomane <sanamori@msn.com>
2018-05-19 20:04:49 +0700

    no message

--- server.js
+++ server.js
@@ -316,7 +316,7 @@ app.get('/node/fintechShare/secure/open/:cypher', function (req, res) {
     shareModel.find({Ticket: req.params.cypher}).select({
         "_id": 0 ,"DataImage": 0
     }).then(function(data){
-        console.log("get framwoke data "+ typeof data+"  "+ data+" instance of JSON: "+data instanceof JSON);
+        console.log("get framwoke data "+ typeof data+"  "+ data[0].NameTicker);
         var start = data.StartDate;
         var end = data.EndDate;
         var ticker = data.NameTicker;
