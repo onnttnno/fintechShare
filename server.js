fcfebea9bb86ba912f19d4552f9a10393ef34c6e
Ritthirong Phoomane <sanamori@msn.com>
2018-05-22 01:16:25 +0700

    no message

--- server.js
+++ server.js
@@ -411,7 +411,7 @@ function findNowSpacific(getCollectionStock, res, start, end) {
             cipher.start({
                 iv: iv
             });
-            cipher.update(forge.util.createBuffer(Buffer.from(htmlString).toString('base64')), 'raw');
+            cipher.update(forge.util.createBuffer(htmlString, 'raw'));
             cipher.finish();
 
             var output = forge.util.createBuffer();
@@ -422,38 +422,9 @@ function findNowSpacific(getCollectionStock, res, start, end) {
                 output.putBytes(salt);
             }
             output.putBuffer(cipher.output);
-            cypher = output.getBytes();
+            cypher = output.toHex();
 
             console.log(cypher);
-
-
-
-            // parse salt from input
-            input = forge.util.createBuffer(cypher, 'binary');
-            // skip "Salted__" (if known to be present)
-            input.getBytes('Salted__'.length);
-            // read 8-byte salt
-            var salt = input.getBytes(8);
-
-            // Note: if using "-nosalt", skip above parsing and use
-            // var salt = null;
-
-            // 3DES key and IV sizes
-            var keySize = 24;
-            var ivSize = 8;
-
-            var buffer = forge.util.createBuffer(derivedBytes);
-            var key = buffer.getBytes(keySize);
-            var iv = buffer.getBytes(ivSize);
-
-            var decipher = forge.cipher.createDecipher('3DES-CBC', key);
-            decipher.start({
-                iv: iv
-            });
-            decipher.update(input);
-            var result = decipher.finish(); // check 'result' for true/false
-
-            console.log("dencrypted is : " + decipher.output.getBytes());
 
             res.send(cypher);
 
